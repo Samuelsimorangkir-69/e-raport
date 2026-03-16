20abd1858768b0beb2b038eed4af6c130292acb0
Samuel Simorangkir <samuel_simorangkir@gmmail.com>
2026-03-16 15:16:23 +0700

    first commit

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,11 @@
+
+<html>
+    <div>
+        <h1>List Barang</h1>
+        <ul>
+            <li>Barang 1</li>
+            <p>Kode Barang: {{$id}}</p>
+            <p>Nama Barang: {{$nama}}</p>
+        </ul>
+    </div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,26 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-//Route::get('/', function () {
-// return view('welcome');
-//});
+Route::get('/', function () {
+return view('welcome');
+});
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/user/{id}', function ($id){
+    return "User dengan ID " . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Ini adalah dashboard admin";
+    });
+    Route::get('/users', function () {
+        return "admin users";
+    });
+});
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//     return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
