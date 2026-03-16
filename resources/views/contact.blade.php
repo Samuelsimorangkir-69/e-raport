d6f1a3f92f7118c4f6f96512b1b20158eae3f57e
Samuel Simorangkir <samuel_simorangkir@gmmail.com>
2026-03-16 12:55:36 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+   Jika anda ingin joki silahkan hubungi nomo rini (+62)899 2233 2234<!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+<!-- Simplicity is an acquired taste. - Katharine Gerould -->
+Selamat Datang di di sam store , pemiliki {{$nama}} <br>
+pekerjaan {{$pekerjaan}}<br>
+</div>
